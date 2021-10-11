bf30dd0348135796bfabaad024dee78981a3bf73
helloprav <helloprav@gmail.com>
2021-10-11 20:55:15 +0530

    updated db password storage

--- open-commons-neer/src/bin/db-passwd.bat
+++ open-commons-neer/src/bin/db-passwd.bat
@@ -1,4 +1,5 @@
 set "crt_dir=%~dp0"
 cd ..\mysql-8.0.19-winx64\bin
-mysql -u root --skip-password -e "alter user 'root'@'localhost' identified by 'root';"
+rem mysql -u root --skip-password -e "alter user 'root'@'localhost' identified by 'root';"
+mysql --defaults-extra-file=%crt_dir%/mysql.cnf
 cd %crt_dir%
